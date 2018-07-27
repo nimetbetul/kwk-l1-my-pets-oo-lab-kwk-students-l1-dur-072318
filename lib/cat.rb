427c79ed5c4e8466a099237bb9babe5d5135269f
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-27 13:16:08 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
   # code goes here
+  def initialize (mood)
+    @mood = mood
+  end
+  def walking
+    @walking
+  end
+  
+  def feeding
+    @feeding
+  end
+  
+  def playing
+    @playing
+  end
 end
+Luna = Cat.new 
